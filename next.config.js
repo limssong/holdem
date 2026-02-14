9dda21ea22972ee22ca8ff8e625dc65c81d59235
limssong <limssong0411@gmail.com>
2026-02-14 23:23:07 +0900

    mod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // GitHub Pages 배포 시에만 basePath 설정
+  basePath: process.env.GITHUB_ACTIONS ? '/holdem' : '',
   images: {
     unoptimized: true,
   },
